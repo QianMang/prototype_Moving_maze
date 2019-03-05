6897147084a6f4b2f415168398688f3ba8077547
mangAI <2385914340@qq.com>
2019-03-05 16:25:53 -0500

    v1.0

---
+++ Assets/Script/Difficulty_control.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Difficulty_control : MonoBehaviour {
+    public enum Difficulty{
+        first_time=1,
+        easy=2,
+        hard=3,
+        Challenger = 4,
+    }
+    public Difficulty difficulty = new Difficulty();
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
